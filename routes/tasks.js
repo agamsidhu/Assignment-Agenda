b0cd5a0582bd699a405871ebffb99cd899536a6a
agamsidhu <ass100120@utdallas.edu>
2017-02-09 09:56:39 -0800

    add in mongojs and connect to db. intial get request returns all items in table

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://agam:agam@ds019816.mlab.com:19816/mytasklist_agam', ['tasks']);
+
 
 router.get('/tasks', function(req, res, next){
-    res.send('TASKS API');
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
 });
 
 module.exports = router;
